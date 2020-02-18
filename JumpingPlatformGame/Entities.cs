15296733f5eb80c588390e51fac19ed8374846a8
Marek Nagy <62247071@cuni.cz>
2020-02-18 22:50:17 +0100

    Add project files.

---
+++ JumpingPlatformGame/Entities.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+
+namespace JumpingPlatformGame {
+	class Entity {
+		public virtual Color Color => Color.Black;
+		public WorldPoint Location;
+		public Seconds time;
+		public void Update(Seconds s)
+		{
+			time += s;
+		}
+	}
+
+	class MovableEntity : Entity {
+		public Movement Horizontal, Vertical;
+	}
+
+	class MovableJumpingEntity : MovableEntity {
+	}
+
+	class Joe : MovableEntity {
+		public override string ToString() => "Joe";
+		public override Color Color => Color.Blue;
+	}
+
+	class Jack : MovableEntity {
+		public override string ToString() => "Jack";
+		public override Color Color => Color.LightBlue;
+	}
+
+	class Jane : MovableJumpingEntity {
+		public override string ToString() => "Jane";
+		public override Color Color => Color.Red;
+	}
+
+	class Jill : MovableJumpingEntity {
+		public override string ToString() => "Jill";
+		public override Color Color => Color.Pink;
+	}
+
+}
---
+++ JumpingPlatformGame/Physics.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace JumpingPlatformGame {
+	static class IntExtensions
+	{
+		//public static Seconds Seconds(this int i)
+		//{
+		//	return new Seconds(i);
+		//}
+		public static Meters Meters(this int i)
+		{
+			return new Meters(i);
+		}
+		public static MeterPerSeconds MeterPerSeconds(this int i)
+		{
+			return new MeterPerSeconds(i);
+		}
+	}
+	static class DoubleExtensions
+	{
+		public static MeterPerSeconds MeterPerSeconds(this double d)
+		{
+			return new MeterPerSeconds(d);
+		}
+		public static Seconds Seconds(this double i)
+		{
+			return new Seconds(i);
+		}
+	}
+	struct Seconds
+	{
+		public double Value { get; private set; }
+		public Seconds(double s)
+		{
+			Value = s;
+		}
+		public static Seconds operator +(Seconds s1, Seconds s2)
+		{
+			return new Seconds(s1.Value + s2.Value);
+		}
+	}
+	struct Meters
+	{
+		public int Value;
+		public Meters(int m)
+		{
+			Value = m;
+		}
+		public static Speed operator /(Meters m, Seconds s)
+		{
+			return new Speed((double)m.Value/s.Value);
+		}
+	}
+	struct Speed
+	{
+		double Value;
+		public Speed(double s)
+		{
+			Value = s;
+		}
+	}
+	struct MeterPerSeconds
+	{
+		public double Value;
+		public MeterPerSeconds(double value)
+		{
+			this.Value = value;
+		}
+	}
+	class WorldPoint
+	{
+		public Meters X,Y;
+		
+	}
+	class Movement
+	{
+		public Meters LowerBound, UpperBound;
+		public MeterPerSeconds Speed;
+		public Movement()
+		{
+
+		}
+	}
+}
